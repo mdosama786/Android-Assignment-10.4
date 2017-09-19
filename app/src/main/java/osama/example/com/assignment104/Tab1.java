a33144df76530112511a7f8635b13783c1647efa
Mohammad Osama <mdosama.ansar@gmail.com>
2017-09-19 13:45:39 +0530

    Android-Assignment-10.4
    included screenshot and apk file

---
+++ app/src/main/java/osama/example/com/assignment104/Tab1.java
@@ -0,0 +1,16 @@
+package osama.example.com.assignment104;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class Tab1 extends Activity {
+
+	/** Called when the activity is first created. */
+	  
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.tab1);
+	    }
+}
